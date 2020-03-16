b4444ad491dee9f147ba6703699193ad63084820
Radoslaw Kumorek <radoslaw.kumorek@eset.pl>
2020-03-16 11:44:48 +0100

    trapez area

---
+++ 02-JSCoreConcepts/trapezium.js
@@ -0,0 +1,7 @@
+a = 2;
+b = 6;
+h = 3;
+
+let fun = (a, b, h) => {return h*(a+b)/2};
+
+console.log( "Pole trapezu o bokach " + a + ", " + b + " oraz wysokości " + h + " wynosi " + fun(a, b, h) + ".");
