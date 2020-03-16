91bebd21654d8b3fe9ebf13f824bbef7e6254d44
Radoslaw Kumorek <radoslaw.kumorek@eset.pl>
2020-03-16 11:23:52 +0100

    address.js: formating strings

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -6,10 +6,26 @@ city = 'Kraków';
 zip = '30-781';
 country = 'Poland';
 
+/*nie stosując formatowania, czyli zwykle wypisanie */
+console.log("nie stosując formatowania, czyli zwykle wypisanie")
+console.log(title)
+console.log(name)
+console.log(surname)
+console.log(street)
+console.log(city)
+console.log(zip)
+console.log(country)
 
+/*stosując formatowania*/
 /* 
 dr Mateusz Kubicki
 ul. Długa 15
 30-781 Kraków
 POLAND
 */
+console.log("\nstosując formatowania")
+console.log(title + " " + name + " " + surname)
+console.log("ul. " + street)
+console.log(zip + " " + "city")
+console.log(country.toUpperCase())
+
